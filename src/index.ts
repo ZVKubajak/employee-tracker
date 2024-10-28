086683ac5cf14c539f6111bc3e19d3a2a5a309bf
VanceGoblin <zvkubajak@gmail.com>
2024-10-28 11:57:50 -0500

    Created Interface w/ Choices to View or Add Fields in schema.sql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
+import { pool, connectToDb } from "./db/connection";
+
+const interaction = async () => {
+  const prompt = [
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add A Department",
+        "Add A Role",
+        "Add An Employee",
+        "Update An Employee Role",
+      ],
+    },
+  ];
+
+  const result = await inquirer.prompt(prompt);
+  switch (result.action) {
+    case "View All Departments":
+      // await viewDepartments();
+      break;
+    case "View All Roles":
+      // await viewRoles();
+      break;
+    case "View All Employees":
+      // await viewEmployees();
+      break;
+    case "Add A Department":
+      // await addDepartment();
+      break;
+    case "Add A Role":
+      // await addRole();
+      break;
+    case "Add An Employee":
+      // await addEmployee();
+      break;
+    case "Update An Employee Role":
+      // await updateEmployeeRole();
+      break;
+    case "Exit":
+      await pool.end();
+      process.exit();
+  }
+
+  interaction();
+};
+
+const startApp = async () => {
+  await connectToDb();
+  interaction();
+};
+
+startApp();
